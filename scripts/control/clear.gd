47d121bf0756783e5c08bc486c4914d5872a730f
Afeef Neiroukh <s44aneir@uni-bonn.de>
2024-08-23 11:33:20 +0200

    refactor...

--- scripts/control/clear.gd
+++ scripts/control/clear.gd
@@ -5,7 +5,7 @@ func _on_pressed():
 	get_tree().call_group("vertex_group", "queue_free")
 	controller.set_mode(controller.modes.vertices)
 	
-	for i in range(vertex_class.node_count):
-		get_tree().call_group("edge_group" + str(i), "queue_free")
+	for id in range(vertex_class.node_count):
+		get_tree().call_group("edge_group" + str(id), "queue_free")
 		
 	vertex_class.node_count = 0
--- scripts/graph/kante.gd
+++ scripts/graph/kante.gd
@@ -4,36 +4,36 @@ class_name edge_class
 
 var start_knoten: Node2D
 var ziel_knoten: Node2D
-var linie: Line2D
-var kopf: Polygon2D
+var line: Line2D
+var head: Polygon2D
 static var margin: float = 85
 
 static var color_def: Color = Color.WHITE
-static var color_besucht: Color = constants.uni_hellblau_c
+static var color_visited: Color = constants.uni_hellblau_c
 
 # Needed to properly draw an edge, if there exists a counteredge
 var displacement: bool = false
 static var displacement_factor: int = 15
 # Number of points of per quarter circle
-static var n: int = 20
+static var points_per_quarter_circle: int = 20
 # Radius of circle
-static var r: float = 50
+static var radius: float = 50
 
-static var bereits_berechnet = false
+static var already_calculated = false
 static var points_origin = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if bereits_berechnet == false:
-		for i in range(n + 1):
-			var angle: float = (float)(2.0 * PI * i) / (n + (n/4.0));
-			var y: float = cos(angle) * r
-			var x: float = sin(angle) * r
+	if already_calculated == false:
+		for i in range(points_per_quarter_circle + 1):
+			var angle: float = (float)(2.0 * PI * i) / (points_per_quarter_circle + (points_per_quarter_circle/4.0));
+			var y: float = cos(angle) * radius
+			var x: float = sin(angle) * radius
 			points_origin.push_back(Vector2(x, y))
-		bereits_berechnet = true
+		already_calculated = true
 			
-	linie = get_node("./Line2D")
-	kopf = get_node("./Polygon2D")
+	line = get_node("./Line2D")
+	head = get_node("./Polygon2D")
 
 func _process(_delta):
 	draw()
@@ -52,31 +52,31 @@ func draw():
 			
 		ziel_punkt = start_punkt + direction * (distance - margin)
 		
-		linie.set_point_position(0, start_punkt)
-		linie.set_point_position(1, ziel_punkt)
-		kopf.position = ziel_punkt
-		kopf.rotation = start_punkt.angle_to_point(ziel_punkt)
+		line.set_point_position(0, start_punkt)
+		line.set_point_position(1, ziel_punkt)
+		head.position = ziel_punkt
+		head.rotation = start_punkt.angle_to_point(ziel_punkt)
 		return
-		
+
 	if start_knoten == ziel_knoten:
-		linie.clear_points()
+		line.clear_points()
 			
 		var points = []
 		# start_knoten.position.y
-		for i in range(0, n):
+		for i in range(0, points_per_quarter_circle):
 			points.push_back(Vector2(points_origin[i].x + start_knoten.position.x, points_origin[i].y + start_knoten.position.y - 65))
 			
 		for i in range(points.size()):
-			linie.add_point(points[i], i)
+			line.add_point(points[i], i)
 			
-		var start_position = Vector2(start_knoten.position.x - r, start_knoten.position.y - 69)
-		kopf.position = start_position
+		var start_position = Vector2(start_knoten.position.x - radius, start_knoten.position.y - 69)
+		head.position = start_position
 		return
 
 func mark_visited():
-	linie.default_color = color_besucht
-	kopf.color = color_besucht
+	line.default_color = color_visited
+	head.color = color_visited
 
 func reset_color():
-	linie.default_color = color_def
-	kopf.color = color_def
+	line.default_color = color_def
+	head.color = color_def
