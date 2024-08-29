c1a6d408f8ffc69e5e5f42f717aec18dc50b6c0a
Afeef Neiroukh <s44aneir@uni-bonn.de>
2024-08-29 13:02:21 +0200

    edges get marked upon visit

--- scripts/algorithms/dfs.gd
+++ scripts/algorithms/dfs.gd
@@ -10,6 +10,7 @@ var s: vertex_class
 var states: Array = []
 var state: int = 0
 var current_stack: Array = []
+var visited_edges = []
 var call_counter: int = 0
 
 # We will save dictionaries in the states Array. These will have these keys:
@@ -18,6 +19,7 @@ enum dfs_keys
 	call_id,
 	stack,
 	last_pop,
+	visited_edges,
 	F,
 	F_2,
 	line_nr
@@ -29,13 +31,14 @@ func algorithm(start_knoten: vertex_class):
 	s = start_knoten
 	states.clear()
 	current_stack.clear()
+	visited_edges.clear()
 	call_counter = 0
 
 	tiefensuche(start_knoten)
 
 	state = 0
 	draw_stack(states[state].get(dfs_keys.stack))
-	return states.size() + 1
+	return states.size()
 
 
 func tiefensuche(start_vertex: vertex_class) -> Array:
@@ -50,6 +53,7 @@ func tiefensuche(start_vertex: vertex_class) -> Array:
 	var dict_beginning: Dictionary
 	dict_beginning[dfs_keys.call_id] = call_id
 	dict_beginning[dfs_keys.stack] = current_stack.duplicate()
+	dict_beginning[dfs_keys.visited_edges] = visited_edges.duplicate()
 	#dict_beginning[dfs_keys.last_pop] = null
 	#dict_beginning[dfs_keys.F] = null
 	#dict_beginning[dfs_keys.F_2] = null
@@ -67,21 +71,25 @@ func tiefensuche(start_vertex: vertex_class) -> Array:
 	
 	for outgoing_edge: edge_class in get_tree().get_nodes_in_group("edge_group" + str(start_vertex.id_)):
 		if not outgoing_edge.target_vertex.visited:
-			print("F = " + misc.int_array_to_string(F) + "(" + str(call_id) + ")")
-			F_2 = tiefensuche(outgoing_edge.target_vertex)
-			print("F' = " + misc.int_array_to_string(F_2))
-			F.append_array(F_2)
-			print("F = " + misc.int_array_to_string(F) + "(" + str(call_id) + ")")
-			# Every loop is a state, create a dict for it
+			visited_edges.push_back(outgoing_edge)
+			
 			var dict_loop: Dictionary
 			dict_loop[dfs_keys.call_id] = call_id
 			dict_loop[dfs_keys.stack] = current_stack.duplicate()
+			dict_loop[dfs_keys.visited_edges] = visited_edges.duplicate()
+			states.push_back(dict_loop)
+			
+			F_2 = tiefensuche(outgoing_edge.target_vertex)
+			F.append_array(F_2)
+			
+			var dict_loop_2: Dictionary
+			dict_loop_2[dfs_keys.call_id] = call_id
+			dict_loop_2[dfs_keys.stack] = current_stack.duplicate()
+			dict_loop_2[dfs_keys.visited_edges] = visited_edges.duplicate()
 			#dict_loop[dfs_keys.last_pop] = null
 			#dict_loop[dfs_keys.F] = F
 			#dict_loop[dfs_keys.F_2] = F_2
-			
-			# Push the new state
-			states.push_back(dict_loop)
+			states.push_back(dict_loop_2)
 	
 	# We will return, so pop the stack
 	current_stack.pop_front()
@@ -90,26 +98,33 @@ func tiefensuche(start_vertex: vertex_class) -> Array:
 	var dict_end: Dictionary
 	dict_end[dfs_keys.call_id] = call_id
 	dict_end[dfs_keys.stack] = current_stack.duplicate()
+	dict_end[dfs_keys.visited_edges] = visited_edges.duplicate()
 	states.push_back(dict_end)
 	
 	return F
 
 func stop():
+	get_tree().call_group("vertex_group", "reset_visited")
 	s.label_id.set_text(str(s.id_))
 	$own_gui.visible = false
 
 @export var stackspeicher: VBoxContainer
+#@export 
+
 func forward():
+	state += 1
+	
+	update_visited_edges()
 	var current_stack_frame: Array = states[state].get(dfs_keys.stack)
 	draw_stack(current_stack_frame)
 	
-	print(state)
-	
-	state += 1
 
 func backward():
 	state -= 1
 
+	update_visited_edges()
+	var current_stack_frame: Array = states[state].get(dfs_keys.stack)
+	draw_stack(current_stack_frame)
 
 func draw_stack(stack: Array):
 	# Clear all stack frames before redrawing
@@ -124,3 +139,9 @@ func draw_stack(stack: Array):
 		# LOCAL FIX
 		stack_frame_instance.get_child(0).text = "Tiefensuche (Aufruf " + str(stack_frame_id) +  ")"
 		stackspeicher.add_child(stack_frame_instance)
+
+func update_visited_edges():
+	for i in range(vertex_class.node_count):
+		get_tree().call_group("edge_group" + str(i), "reset_color")
+	for edge: edge_class in states[state].get(dfs_keys.visited_edges):
+		edge.mark_visited()
