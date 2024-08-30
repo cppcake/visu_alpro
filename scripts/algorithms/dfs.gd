31eed95d35f8639317703821c3a573b3cde2950f
Afeef Neiroukh <s44aneir@uni-bonn.de>
2024-08-30 07:48:15 +0200

    DFS Labels get updated

--- scripts/algorithms/dfs.gd
+++ scripts/algorithms/dfs.gd
@@ -17,12 +17,12 @@ var call_counter: int = 0
 enum dfs_keys
 {
 	call_id,
-	stack,
-	last_pop,
-	visited_edges,
 	F,
 	F_2,
-	line_nr
+	line_nr,
+	last_pop,
+	stack,
+	visited_edges
 }
 
 func algorithm(start_knoten: vertex_class):
@@ -37,7 +37,7 @@ func algorithm(start_knoten: vertex_class):
 	tiefensuche(start_knoten)
 
 	state = 0
-	draw_stack(states[state].get(dfs_keys.stack))
+	update_visuals()
 	return states.size()
 
 
@@ -49,57 +49,34 @@ func tiefensuche(start_vertex: vertex_class) -> Array:
 	# Update the current stackframe
 	current_stack.push_front(call_id)
 	
-	# The beginning of a call is a state, create a dict for it
-	var dict_beginning: Dictionary
-	dict_beginning[dfs_keys.call_id] = call_id
-	dict_beginning[dfs_keys.stack] = current_stack.duplicate()
-	dict_beginning[dfs_keys.visited_edges] = visited_edges.duplicate()
-	#dict_beginning[dfs_keys.last_pop] = null
-	#dict_beginning[dfs_keys.F] = null
-	#dict_beginning[dfs_keys.F_2] = null
-	
-	# Push the new state
-	states.push_back(dict_beginning)
+	# The beginning of a call is a state, create a state for it
+	store_state(call_id, null, null)
 	
 	# Create empty sequences F and F_2
 	var F: Array
 	var F_2: Array
 	
+	store_state(call_id - 1, F, F_2)
 	# Mark start_vertex as visited and push_back in F
 	start_vertex.visited = true
 	F.push_back(start_vertex)
 	
+	store_state(call_id - 1, F, F_2)
 	for outgoing_edge: edge_class in get_tree().get_nodes_in_group("edge_group" + str(start_vertex.id_)):
+		visited_edges.push_back(outgoing_edge)
+		store_state(call_id, F, F_2)
 		if not outgoing_edge.target_vertex.visited:
-			visited_edges.push_back(outgoing_edge)
-			
-			var dict_loop: Dictionary
-			dict_loop[dfs_keys.call_id] = call_id
-			dict_loop[dfs_keys.stack] = current_stack.duplicate()
-			dict_loop[dfs_keys.visited_edges] = visited_edges.duplicate()
-			states.push_back(dict_loop)
-			
 			F_2 = tiefensuche(outgoing_edge.target_vertex)
+			store_state(call_id - 1, F, F_2)
 			F.append_array(F_2)
-			
-			var dict_loop_2: Dictionary
-			dict_loop_2[dfs_keys.call_id] = call_id
-			dict_loop_2[dfs_keys.stack] = current_stack.duplicate()
-			dict_loop_2[dfs_keys.visited_edges] = visited_edges.duplicate()
-			#dict_loop[dfs_keys.last_pop] = null
-			#dict_loop[dfs_keys.F] = F
-			#dict_loop[dfs_keys.F_2] = F_2
-			states.push_back(dict_loop_2)
+			store_state(call_id, F, F_2)
 	
+	
+
 	# We will return, so pop the stack
 	current_stack.pop_front()
 	
-	# The beginning of a call is a state, create a dict for it
-	var dict_end: Dictionary
-	dict_end[dfs_keys.call_id] = call_id
-	dict_end[dfs_keys.stack] = current_stack.duplicate()
-	dict_end[dfs_keys.visited_edges] = visited_edges.duplicate()
-	states.push_back(dict_end)
+	store_state(call_id - 1, F, F_2)
 	
 	return F
 
@@ -114,17 +91,25 @@ func stop():
 func forward():
 	state += 1
 	
-	update_visited_edges()
-	var current_stack_frame: Array = states[state].get(dfs_keys.stack)
-	draw_stack(current_stack_frame)
+	update_visuals()
 	
-
 func backward():
 	state -= 1
 
-	update_visited_edges()
-	var current_stack_frame: Array = states[state].get(dfs_keys.stack)
-	draw_stack(current_stack_frame)
+	update_visuals()
+
+func store_state(call_id: int, F, F_2):
+	var dict: Dictionary
+	dict[dfs_keys.call_id] = call_id
+	if F != null:
+		dict[dfs_keys.F] = F.duplicate()
+		dict[dfs_keys.F_2] = F_2.duplicate()
+	else:
+		dict[dfs_keys.F] = F
+		dict[dfs_keys.F_2] = F_2
+	dict[dfs_keys.stack] = current_stack.duplicate()
+	dict[dfs_keys.visited_edges] = visited_edges.duplicate()
+	states.push_back(dict)
 
 func draw_stack(stack: Array):
 	# Clear all stack frames before redrawing
@@ -140,8 +125,40 @@ func draw_stack(stack: Array):
 		stack_frame_instance.get_child(0).text = "Tiefensuche (Aufruf " + str(stack_frame_id) +  ")"
 		stackspeicher.add_child(stack_frame_instance)
 
+@export var label_call: Label
+@export var label_sequence: Label
+@export var label_sequence_2: Label
+func update_code_labels(call_id: int, F, F_2):
+	var dfs: String = tr("DEPHT-FIRST-SEARCH")
+	if call_id == 0:
+		label_call.text = dfs
+	else:
+		var call: String = tr("CALL")
+		label_call.text = dfs + " (" + call + " " + str(call_id) + " )"
+		
+	if F == null:
+		label_sequence.visible = false
+		label_sequence_2.visible = false
+		return
+	
+	label_sequence.visible = true
+	label_sequence.text = "F = " + misc.int_array_to_string(F)
+	
+	label_sequence_2.visible = true
+	label_sequence_2.text = "F' = " +  misc.int_array_to_string(F_2)
+
 func update_visited_edges():
 	for i in range(vertex_class.node_count):
 		get_tree().call_group("edge_group" + str(i), "reset_color")
 	for edge: edge_class in states[state].get(dfs_keys.visited_edges):
 		edge.mark_visited()
+
+func update_visuals():
+	var current_stack_frame: Array = states[state].get(dfs_keys.stack)
+	var call_id: int = states[state].get(dfs_keys.call_id)
+	var F = states[state].get(dfs_keys.F)
+	var F_2 = states[state].get(dfs_keys.F_2)
+	
+	update_visited_edges()
+	draw_stack(current_stack_frame)
+	update_code_labels(call_id, F, F_2)
