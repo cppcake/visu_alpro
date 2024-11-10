88d30f37b27ff16aa4140277921bdad412367fd9
Afeef Neiroukh <s44aneir@uni-bonn.de>
2024-11-10 13:15:29 +0100

    Vertices now display their refrence_counter

--- graph/list_vertex.gd
+++ graph/list_vertex.gd
@@ -6,6 +6,7 @@ static var lerp_speed: int = 15;
 # Visuals of vertex 
 var sprite: Sprite2D
 var label_data: Label
+var label_ref_count: Label
 var p1: pointer_class
 
 # Animation
@@ -14,7 +15,8 @@ var dest_pos
 func _ready():
 	sprite = get_child(1)
 	label_data = get_child(2)
-	p1 = get_child(3)
+	label_ref_count = get_child(3)
+	p1 = get_child(4)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -26,10 +28,15 @@ var reference_counter: int = 0
 @export var sprite_default: CompressedTexture2D
 @export var sprite_to_remove: CompressedTexture2D
 func _process(_delta):
+	# Not efficient i dont care
 	if reference_counter >= 1:
 			sprite.texture = sprite_default
+			label_ref_count.add_theme_color_override("font_color", constants.ok_green_c)
 	else:
 			sprite.texture = sprite_to_remove
+			label_ref_count.add_theme_color_override("font_color", constants.nope_red_c)
+	
+	label_ref_count.text = str(reference_counter)
 
 func move_to(dest_pos_: Vector2):
 	if dest_pos != null:
--- misc/constants.gd
+++ misc/constants.gd
@@ -15,4 +15,10 @@ static var uni_gelb_c = Color(0.988, 0.729, 0)
 static var uni_grau = "#909085"
 static var uni_grau_c = Color(0.565, 0.565, 0.522)
 
+static var ok_green = "#41a145"
+static var ok_green_c = Color(0.255, 0.631, 0.271)
+
+static var nope_red = "#b85450"
+static var nope_red_c = Color(0.722, 0.329, 0.314)
+
 static var hovered = Color(0.85, 0.85, 0.85, 1.0)
