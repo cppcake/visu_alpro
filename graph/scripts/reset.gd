895db952630ecb2eb110a154913c5925fd316f4d
Afeef Neiroukh <s44aneir@uni-bonn.de>
2024-11-11 08:15:30 +0100

    Work on new side panel

---
+++ graph/scripts/reset.gd
@@ -0,0 +1,14 @@
+extends Button
+@export var controller: Node
+@export var camera: Camera2D
+
+func _on_pressed():
+	get_tree().call_group("vertex_group", "queue_free")
+	controller.set_mode(controller.modes.vertices)
+	
+	for id in range(vertex_class.node_count):
+		get_tree().call_group("edge_group" + str(id), "queue_free")
+	
+	vertex_class.node_count = 0
+	
+	camera.reset_zoom()
