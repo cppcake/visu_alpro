486b9e6f070db0213530ea16c5ad6ebf6b783902
Afeef Neiroukh <s44aneir@uni-bonn.de>
2024-11-11 11:42:01 +0100

    Small changes

--- topics/ui/scripts/buttons_toggle.gd
+++ topics/ui/scripts/buttons_toggle.gd
@@ -5,21 +5,25 @@ func _on_pressed():
 	if is_in_group("buttons_active"):
 		# Block active buttons, unblock navigation buttons
 		get_tree().call_group("buttons_active", "release_focus")
-		for button: Button in get_tree().get_nodes_in_group("buttons_active"):
-			button.disabled = true
+		for button: Control in get_tree().get_nodes_in_group("buttons_active"):
+			if button is Button:
+				button.disabled = true
 			button.mouse_filter = button.MOUSE_FILTER_IGNORE
 			
-		for button: Button in get_tree().get_nodes_in_group("buttons_navigation"):
-			button.disabled = false
+		for button: Control in get_tree().get_nodes_in_group("buttons_navigation"):
+			if button is Button:
+				button.disabled = false
 			button.mouse_filter = button.MOUSE_FILTER_STOP
 	
 	if is_in_group("buttons_navigation"):
 		# Unblock active buttons, block navigation buttons
 		get_tree().call_group("buttons_navigation", "release_focus")
-		for button: Button in get_tree().get_nodes_in_group("buttons_active"):
-			button.disabled = false
+		for button: Control in get_tree().get_nodes_in_group("buttons_active"):
+			if button is Button:
+				button.disabled = false
 			button.mouse_filter = button.MOUSE_FILTER_STOP
 			
 		for button: Button in get_tree().get_nodes_in_group("buttons_navigation"):
-			button.disabled = true
+			if button is Button:
+				button.disabled = true
 			button.mouse_filter = button.MOUSE_FILTER_IGNORE
