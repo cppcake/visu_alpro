f0d6614bb62c9c79a2b1a77687ee40e1d1af0319
Afeef Neiroukh <s44aneir@uni-bonn.de>
2024-11-17 17:35:10 +0100

    Progress insert_after on dll

--- topics/doubly_list/doubly_list.gd
+++ topics/doubly_list/doubly_list.gd
@@ -106,21 +106,31 @@ func _on_button_insert_front_pressed():
 func insert_after(step: int):
 	var pred: list_vertex_class = list_vertex_class.selected_vertex
 	match step:
+		0:
+			pass
 		1:
-			make_shared(pred.position + Vector2(125, -175), "above")
-			side_panel.highlight_code([1])
-		2:
-			new_vertex.p1.set_target(pred.p1.target)
-			side_panel.highlight_code([2])
-		3:
-			pred.p1.set_target(new_vertex)
-			side_panel.highlight_code([3])
-		4:
 			size += 1
-			side_panel.highlight_code([4])
+			highlight_code([1])
+		2:
+			make_shared(pred.position + Vector2(125, -250))
+			highlight_code([2])
+		3:
+			new_vertex.p1.set_target(pred.p1.target)
+			highlight_code([3])
+		4:
+			new_vertex.p2.set_target(pred)
+			highlight_code([4])
 		5:
-			side_panel.highlight_code([5])
+			highlight_code([6])
+		6:
+			pred.p1.target.p2.set_target(new_vertex)
+			highlight_code([7])
+		7:
+			pred.p1.set_target(new_vertex)
+			highlight_code([9])
+		8:
 			side_panel.override_code_return()
+			highlight_code([10])
 func insert_after_b(step: int):
 	var pred: list_vertex_class = list_vertex_class.selected_vertex
 	match step:
@@ -138,10 +148,10 @@ func insert_after_b(step: int):
 		1:
 			unshare()
 func _on_button_insert_after_pressed():
-	side_panel.override_code(tr("INS_AFTER"))
+	side_panel.override_code(tr("INS_AFTER_DL"))
 	side_panel.override_code_call("list.insert_after(ListNodeptr pred, data)")
 	side_panel.override_exp("Pick a predecessor Node")
-	set_up(5, insert_after, insert_after_b, true)
+	set_up(8, insert_after, insert_after_b, true)
 
 func remove_after(step: int):
 	var pred: list_vertex_class = list_vertex_class.selected_vertex
