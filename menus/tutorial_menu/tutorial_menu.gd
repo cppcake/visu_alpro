27a72b03b52cabc5f76de7583541166dedce1cdc
Afeef Neiroukh <s44aneir@uni-bonn.de>
2024-12-18 07:31:54 +0100

    Work on tutorial

---
+++ menus/tutorial_menu/tutorial_menu.gd
@@ -0,0 +1,10 @@
+extends CenterContainer
+
+@export var vbox_content: VBoxContainer
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	get_child(0).custom_minimum_size = Vector2(350, vbox_content.get_child_count() * 42)
+	var esc = Input.is_action_just_pressed("ESC")
+	if esc:
+		visible = !visible
